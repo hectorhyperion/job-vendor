a88999c73dc607ffde9aeb1d61c3007333b37b90
hectorhyperion <agimqwertyakomaye@gmail.com>
2022-09-29 13:33:12 +0100

    not new

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -43,8 +43,7 @@
                 <li>
                     <a href="/listing/manage" class="hover:text-laravel"
                         ><i class="fa-solid fa-gear"></i>
-                        Manage Listings</a
-                    >
+                        Manage Listings</a >
                 </li>
                     <li>
                         <form class="inline" method="POST" action="/logout">
@@ -75,15 +74,11 @@
      {{$slot}}
 </main>
 
-<footer
-class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
+<footer class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
 >
 <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
 
-<a
-    href="/listing/create"
-    class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
-    >Post Job</a>
+<a href="/listing/create"class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Job</a>
 </footer>
 <x-flash-message />
 </body>
